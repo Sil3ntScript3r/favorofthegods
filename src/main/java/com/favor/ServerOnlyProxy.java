380733000106491c98d70eeef197161f887e8238
Sil3nt <sil3ntscript3r@gmail.com>
2015-01-20 18:04:54 -0500

    Added Base Mod
    Added Base Mod class, and the three proxies.
    Also loaded in Eclipse, which is all the file changes.

---
+++ src/main/java/com/favor/ServerOnlyProxy.java
@@ -0,0 +1,33 @@
+package com.favor;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+public class ServerOnlyProxy extends CommonProxy {
+	public void preInit()
+	{
+		super.preInit();
+	}
+	
+	public void init()
+	{
+		super.init();
+	}
+	
+	public void postInit()
+	{
+		super.postInit();
+	}
+	
+	public boolean playerIsInCreativeMode(EntityPlayer player)
+	{
+		if(player instanceof EntityPlayerMP)
+		{
+			EntityPlayerMP playerMP = (EntityPlayerMP)player;
+			return playerMP.theItemInWorldManager.isCreative();
+		}
+		return false;
+	}
+}
