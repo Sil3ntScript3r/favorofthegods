fd3fecffdc31ef9e3aba91c882b7616642a2845d
Sil3nt <sil3ntscript3r@gmail.com>
2015-02-20 18:14:30 -0500

    Will only show up to 6 gods a screen More pages not set up, it just won't show past the 6th god

--- src/main/java/com/favorofthegods/gui/GUIAltar.java
+++ src/main/java/com/favorofthegods/gui/GUIAltar.java
@@ -1,6 +1,7 @@
 package com.favorofthegods.gui;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.BlockPos;
@@ -13,8 +14,11 @@ import com.favorofthegods.favornetwork.FavorHandler;
 import com.favorofthegods.gods.Gods;
 
 public class GUIAltar extends GuiScreen {
+	private static final int bgWidth = 256;
+	private static final int bgHeight = 170;
+	private static final int favorYChange = 50;
+	
 	private int x, y, z;
-	private int xSize, ySize;
 	private EntityPlayer player;
 	private World world;
 	private ResourceLocation backgroundImage;
@@ -26,17 +30,21 @@ public class GUIAltar extends GuiScreen {
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		xSize = 256;
-		ySize = 170;
 
 		backgroundImage = new ResourceLocation(FavorOfTheGods.MODID + ":" + "textures/gui/GuiAltar.png");
+	}
+	
+	public void initGui()
+	{
+		this.buttonList.clear();
 	}
 	
 	public void drawScreen(int mouseX, int mouseY, float renderPartialTicks)
 	{
         super.drawScreen(mouseX, mouseY, renderPartialTicks);
-		int x = (width - xSize) / 2;
-		int y = (height - ySize) / 2;
+		int x = (width - bgWidth) / 2;
+		int y = (height - bgHeight) / 2;
+		int favorY = y + 30;
 		int leftX = x + 8;
 		int rightX = x + 141;
 		int Y1 = y + 30;
@@ -45,37 +53,81 @@ public class GUIAltar extends GuiScreen {
 
 		Minecraft.getMinecraft().getTextureManager().bindTexture(backgroundImage);
 		
-		drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+		drawTexturedModalRect(x, y, 0, 0, bgWidth, bgHeight);
 
+		// TODO: Add additional pages to show past 6 gods
+		// Need more gods to set this up completely
+		int godCountBox = 0;
+		int godCountText = 0;
+		double godsOnPage;
+
+		if(FavorHandler.getFavorSize(player) < 6)
+			godsOnPage = FavorHandler.getFavorSize(player);
+		else
+			godsOnPage = 6;
+		
 		// Draw all the boxes and the indicator
-		for(int i = 0; i < FavorHandler.getFavorSize(player); i++)
+		for(int c = 0; c < Math.ceil(godsOnPage / 2); c++)
 		{
-			if(i % 2 == 0)
+			if(c == Math.ceil(godsOnPage / 2) - 1 && godsOnPage % 2 == 1)
 			{
-				drawTexturedModalRect(leftX + 2, Y1 + 3, 0, 198, 102, 21); 											// Background
-				drawTexturedModalRect((leftX + 2) + 50 + (int)(altar.getFavor(i) / 200), Y1 + 3, 0, 235, 2, 21); 	// Cursor
-				drawTexturedModalRect(leftX, Y1, 0, 170, 107, 26);													// Outline
+				drawTexturedModalRect(leftX + 2, (favorY + (favorYChange * c)) + 3, 0, 198, 102, 21); 													// Background
+				drawTexturedModalRect((leftX + 2) + 50 + (int)(altar.getFavor(godCountBox) / 200), (favorY + (favorYChange * c)) + 3, 0, 235, 2, 21); 	// Indicator
+				drawTexturedModalRect(leftX, (favorY + (favorYChange * c)), 0, 170, 107, 26);															// Outline
+				//godCountBox++;
+				godCountBox = 0;
+				break;
 			}
-			else
+			
+			for(int i = 0; i < 2; i++)
 			{
-				drawTexturedModalRect(rightX + 2, Y1 + 3, 0, 198, 102, 21);
-				drawTexturedModalRect((rightX + 2) + 50 + (int)(altar.getFavor(i) / 200), Y1 + 3, 0, 235, 2, 21);
-				drawTexturedModalRect(rightX, Y1, 0, 170, 107, 26);
+				if(i % 2 == 0)
+				{
+					drawTexturedModalRect(leftX + 2, (favorY + (favorYChange * c)) + 3, 0, 198, 102, 21); 													// Background
+					drawTexturedModalRect((leftX + 2) + 50 + (int)(altar.getFavor(godCountBox) / 200), (favorY + (favorYChange * c)) + 3, 0, 235, 2, 21); 	// Indicator
+					drawTexturedModalRect(leftX, (favorY + (favorYChange * c)), 0, 170, 107, 26);															// Outline
+					//godCountBox++;
+					godCountBox = 1;
+				}
+				else
+				{
+					drawTexturedModalRect(rightX + 2, (favorY + (favorYChange * c)) + 3, 0, 198, 102, 21);
+					drawTexturedModalRect((rightX + 2) + 50 + (int)(altar.getFavor(godCountBox) / 200), (favorY + (favorYChange * c)) + 3, 0, 235, 2, 21);
+					drawTexturedModalRect(rightX, (favorY + (favorYChange * c)), 0, 170, 107, 26);
+					//godCountBox++;
+					godCountBox = 0;
+				}
 			}
 		}
 		
 		// Draw all the text [FOR SOME REASON CAN'T DO THIS WITH THE BOXES?]
-		for(int i = 0; i < FavorHandler.getFavorSize(player); i++)
+		for(int c = 0; c < Math.ceil(godsOnPage / 2); c++)
 		{
-			if(i % 2 == 0)
+			if(c == Math.ceil(godsOnPage / 2) - 1 && godsOnPage % 2 == 1)
 			{
-				drawCenteredString(this.fontRendererObj, Gods.godNames.get(i), leftX + 52, Y1 + 4, 0x00999999);
-				drawCenteredString(this.fontRendererObj, "" + altar.getFavor(i), leftX + 52, Y1 + 16, 0x00999999);
+				drawCenteredString(this.fontRendererObj, Gods.godNames.get(godCountText), leftX + 52, (favorY + (favorYChange * c)) + 4, 0x00999999);
+				drawCenteredString(this.fontRendererObj, "" + altar.getFavor(godCountText), leftX + 52, (favorY + (favorYChange * c)) + 16, 0x00999999);
+				//godCountText++;
+				godCountText = 1;
+				break;
 			}
-			else
+			
+			for(int i = 0; i < 2; i++)
 			{
-				drawCenteredString(this.fontRendererObj, Gods.godNames.get(i), rightX + 52, Y1 + 4, 0x00999999);
-				drawCenteredString(this.fontRendererObj, "" + altar.getFavor(i), rightX + 52, Y1 + 16, 0x00999999);
+				if(i % 2 == 0)
+				{
+					drawCenteredString(this.fontRendererObj, Gods.godNames.get(godCountText), leftX + 52, (favorY + (favorYChange * c)) + 4, 0x00999999);
+					drawCenteredString(this.fontRendererObj, "" + altar.getFavor(godCountText), leftX + 52, (favorY + (favorYChange * c)) + 16, 0x00999999);
+					//godCountText++;
+					godCountText = 1;
+				}
+				else
+				{
+					drawCenteredString(this.fontRendererObj, Gods.godNames.get(godCountText), rightX + 52, (favorY + (favorYChange * c)) + 4, 0x00999999);
+					drawCenteredString(this.fontRendererObj, "" + altar.getFavor(godCountText), rightX + 52, (favorY + (favorYChange * c)) + 16, 0x00999999);
+					//godCountText++;
+					godCountText = 0;
+				}
 			}
 		}
 	}
